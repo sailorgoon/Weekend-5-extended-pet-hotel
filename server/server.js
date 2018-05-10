443cca04e1a9eefccffda090dbd2f5d12631000d
Kristen Hare <sailorgoon@gmail.com>
2018-05-10 10:28:35 -0500

    folders and files created, listening on port 5000

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('server/public'));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
