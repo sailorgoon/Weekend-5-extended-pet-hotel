7adc7d9a9e4cf2e86edd55facd7e80995a11e651
Kristen Hare <sailorgoon@gmail.com>
2018-05-10 11:47:59 -0500

    get req moved to pet service

--- server/public/scripts/controllers/pet.controller.js
+++ server/public/scripts/controllers/pet.controller.js
@@ -1,34 +1,9 @@
-app.controller('PetController', ['$http', function ($http) {
+app.controller('PetController', ['PetService', function (PetService) {
     console.log('PetController has been loaded');
     const self = this;
 
     self.message = 'PetsPetsPets'
-
-    self.getAllPets = function() {
-        $http({
-            method: 'GET',
-            url:'/pet'
-        }).then((response) => {
-            console.log('response', response);
-            self.pets = response.data;
-        })
-        .catch((error) => {
-            console.log('error making rent get request', error);
-            // alert('Something went wrong! Check the server.');
-        });
-    }
-    self.getAllPets();
-    // self.ships = SpaceShipService.ships;
-    // self.assignments = SpaceShipService.assignments;
-
-    // self.crewToAdd = {name:'', role:'', ship_id:''};
-
-    // self.addCrewMember = function (crewMember) {
-    //     SpaceShipService.addCrewMember(crewMember);
-    //     self.crewToAdd = { name: '', role: '', ship_id: '' };
-    // };
-
-    // self.removeCrewMember = function (crewMemberId) {
-    //     SpaceShipService.removeCrewMember(crewMemberId);
-    // }
+    
+    self.pets = PetService.pets
+   
 }]);
---
+++ server/public/scripts/services/pet.service.js
@@ -0,0 +1,22 @@
+app.service('PetService', ['$http', function($http) {
+    console.log('PetService has been loaded');
+    const self = this;
+
+    self.pets = { list: [] };
+
+    self.getAllPets = function() {
+        $http({
+            method: 'GET',
+            url:'/pet'
+        }).then((response) => {
+            console.log('response', response);
+            self.pets.list = response.data;
+        })
+        .catch((error) => {
+            console.log('error making rent get request', error);
+            // alert('Something went wrong! Check the server.');
+        });
+    }
+    self.getAllPets();
+    
+}]);
