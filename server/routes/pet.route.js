97446eb5411d12c3dbf4c0dafe3f5796df0bf87f
Kristen Hare <sailorgoon@gmail.com>
2018-05-10 11:14:12 -0500

    server side GET route getting from database

--- server/routes/pet.route.js
+++ server/routes/pet.route.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+    console.log('GET /pet');
+    pool.query(`SELECT * FROM "pets"`)
+        .then(result => {
+            // console.log(result);
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('ERROR SELECTING SHIPS - GET /pet -', error);
+            res.sendStatus(500);
+        });
+});
+
 module.exports = router;
