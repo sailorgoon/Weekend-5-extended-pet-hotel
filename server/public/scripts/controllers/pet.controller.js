728e985a5b7326d7a136fb6a728500c0e6df8979
Kristen Hare <sailorgoon@gmail.com>
2018-05-10 11:30:07 -0500

    client side get working, ng-repeating on dom

--- server/public/scripts/controllers/pet.controller.js
+++ server/public/scripts/controllers/pet.controller.js
@@ -1,8 +1,23 @@
-app.controller('PetController', function () {
+app.controller('PetController', ['$http', function ($http) {
     console.log('PetController has been loaded');
     const self = this;
 
     self.message = 'PetsPetsPets'
+
+    self.getAllPets = function() {
+        $http({
+            method: 'GET',
+            url:'/pet'
+        }).then((response) => {
+            console.log('response', response);
+            self.pets = response.data;
+        })
+        .catch((error) => {
+            console.log('error making rent get request', error);
+            // alert('Something went wrong! Check the server.');
+        });
+    }
+    self.getAllPets();
     // self.ships = SpaceShipService.ships;
     // self.assignments = SpaceShipService.assignments;
 
@@ -16,4 +31,4 @@ app.controller('PetController', function () {
     // self.removeCrewMember = function (crewMemberId) {
     //     SpaceShipService.removeCrewMember(crewMemberId);
     // }
-});
+}]);
